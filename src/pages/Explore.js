ed71e01c54da8cb70e07ec4ec75983148431c21e
Eduardo Muchak <eduardomuchak@gmail.com>
2022-04-27 15:51:46 -0300

    fix: Requisitos 67 à 69 finalizados

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 export default function Explore() {
+  const history = useHistory();
   return (
     <>
       <Header />
-      <main>Explore</main>
+      <section>
+        <button
+          type="button"
+          data-testid="explore-foods"
+          onClick={ () => { history.push('/explore/foods'); } }
+        >
+          Explore Foods
+        </button>
+        <button
+          type="button"
+          data-testid="explore-drinks"
+          onClick={ () => { history.push('/explore/drinks'); } }
+        >
+          Explore Drinks
+        </button>
+      </section>
       <Footer />
     </>
   );
