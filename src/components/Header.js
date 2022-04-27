074b63f4dcb12330df8484f752ac8ad682f3917e
Bruno Roja <brunojlee@hotmail.com>
2022-04-27 13:32:46 -0400

    feat: :twisted_rightwards_arrows: Ajuste do Header para merge

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,10 @@
 import React from 'react';
-import SearchBar from './SearchBar';
 
 function Header() {
   return (
-    <>
-      <button
-        data-testid="search-top-btn"
-        type="button"
-      >
-        Eu sou uma lupa
-      </button>
-      <SearchBar />
-    </>
+    <header>
+      Header
+    </header>
   );
 }
 
