1ecbcba88a0a658038533ba71dfe6f52a20a18c5
Eduardo Muchak <eduardomuchak@gmail.com>
2022-04-27 16:29:37 -0300

    fix: Requisitos 82 ao 87 finalizados --Obs: Requisito 83 ainda não passa no teste, pois depende de roteamento

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,12 +1,45 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import RecipesContext from '../context/RecipesContext';
 
 export default function Profile() {
+  const history = useHistory();
+  const { userEmail } = useContext(RecipesContext);
+
+  const clearLocalStorage = () => {
+    localStorage.clear();
+    history.push('/');
+  };
+
   return (
     <>
       <Header />
-      <main>Profile</main>
+      <span data-testid="profile-email">
+        {userEmail}
+      </span>
+      <button
+        type="button"
+        data-testid="profile-done-btn"
+        onClick={ () => { history.push('/done-recipes'); } }
+      >
+        Done Recipes
+      </button>
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+        onClick={ () => { history.push('/favorite-recipes'); } }
+      >
+        Favorite Recipes
+      </button>
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+        onClick={ () => { clearLocalStorage(); } }
+      >
+        Logout
+      </button>
       <Footer />
     </>
   );
