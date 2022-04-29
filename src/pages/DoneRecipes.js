2991ce1c9be4bbf26661a2677451d4f61321a4b9
Leonardo Begnossi <89605267+MadOrchid@users.noreply.github.com>
2022-04-29 16:13:38 -0300

    :construction: Iniciando requisito 54
    Co-authored-by: Rafaela Jordão <rafaela-jordao@users.noreply.github.com>

--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -1,11 +1,54 @@
 import React from 'react';
 import Header from '../components/Header';
+import shareIcon from '../images/shareIcon.svg';
 
 export default function DoneRecipes() {
   return (
     <>
       <Header pageName="Done Recipes" showProfileImg="true" />
-      <main>DoneRecipes</main>
+      <div>
+        <button
+          data-testid="filter-by-all-btn"
+          type="button"
+        >
+          All
+        </button>
+        <button
+          data-testid="filter-by-food-btn"
+          type="button"
+        >
+          Food
+        </button>
+        <button
+          data-testid="filter-by-drink-btn"
+          type="button"
+        >
+          Drinks
+        </button>
+
+        <div>
+          <img
+            alt="Card Receita"
+            data-testid={ `${index}-horizontal-image` }
+          />
+
+          <span
+            data-testid={ `${index}-horizontal-top-text` }
+          />
+          <span
+            data-testid={ `${index}-horizontal-name` }
+          />
+
+          <span
+            data-testid={ `${index}-horizontal-done-date` }
+          />
+          <img
+            data-testid={ `${index}-horizontal-share-btn` }
+            src={ shareIcon }
+            alt="Imagem de compartilhamento"
+          />
+        </div>
+      </div>
     </>
   );
 }
--- src/pages/FoodInProgress.js
+++ src/pages/FoodInProgress.js
@@ -2,9 +2,47 @@ import React from 'react';
 
 function FoodInProgress() {
   return (
-    <h1>
-      Food in Progress Page
-    </h1>
+    <>
+      <h1 data-testid="recipe-title">
+        Food in Progress Page
+      </h1>
+      <img
+        src={ recipe.strMealThumb }
+        alt="Foto da receita"
+        data-testid="recipe-photo"
+      />
+
+      <div>
+        <h3>Ingredients</h3>
+      </div>
+
+      <h2 data-testid="instructions">Instruções</h2>
+
+      <button
+        type="button"
+        data-testid="share-btn"
+        // onClick={}
+      >
+        Compartilhar
+      </button>
+
+      <button
+        type="button"
+        data-testid="favorite-btn"
+        // onClick={}
+      >
+        Favoritar
+      </button>
+
+      <button
+        type="button"
+        data-testid="finish-recipe-btn"
+        // onClick={}
+      >
+        Finalizar receita
+      </button>
+
+    </>
   );
 }
 
