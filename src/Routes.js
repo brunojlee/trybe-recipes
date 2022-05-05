e453d1f1f9f1271d6442ae753db03a626de83f24
Bruno Roja <brunojlee@hotmail.com>
2022-05-05 02:11:27 -0400

    feat: :sparkles: Requerimento 81

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import imports from './imports';
+import NotFound from './pages/NotFound';
 
 const {
   Login,
@@ -40,6 +41,7 @@ function Routes() {
       <Route exact path="/profile" component={ Profile } />
       <Route exact path="/done-recipes" component={ DoneRecipes } />
       <Route exact path="/favorite-recipes" component={ FavoriteRecipes } />
+      <Route path="*" component={ NotFound } />
     </Switch>
   );
 }
