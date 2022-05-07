ed2b7ba449a3a4d07c325b3f17c8dae5831b6285
Bruno Roja <brunojlee@hotmail.com>
2022-05-07 03:45:12 -0400

    test: :construction: Criação dos testes para as páginas Explore by Ingredients

--- src/tests/FoodDetailsPage.test.js
+++ src/tests/FoodDetailsPage.test.js
@@ -4,15 +4,16 @@ import React from 'react';
 import { act } from 'react-dom/test-utils';
 import App from '../App';
 import renderWithBrowser from './Helpers/renderWithBrowser';
+import renderWithRouter from './Helpers/renderWithRouter';
 
 const delay = (ms) => new Promise((res) => setTimeout(res, ms));
 const timeDelay = 2000;
 const arrabiataImgs = 9;
-
+const recipe = '/foods/52771';
 describe('Teste da página de detalhes de comidas ', () => {
   it('Serão validados os componentes.', async () => {
     await act(async () => {
-      renderWithBrowser(<App />).history.push('/foods/52771');
+      renderWithBrowser(<App />).history.push(recipe);
     });
     await act(async () => {
       await delay(timeDelay);
@@ -30,5 +31,15 @@ describe('Teste da página de detalhes de comidas ', () => {
       fireEvent.click(startBtn);
     });
     expect(window.location.pathname).toBe('/foods/52771/in-progress');
+  });
+  it('Será validado o botão de voltar.', async () => {
+    await act(async () => {
+      renderWithRouter(<App />).history.push(recipe);
+    });
+    await act(async () => {
+      await delay(timeDelay);
+      fireEvent.click(screen.getAllByRole('button')[0]);
+    });
+    expect(window.location.pathname).not.toBe(recipe);
   });
 });
