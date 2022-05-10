185316e1aeef5b1aae8705f4fdfe891f58b05e4b
Eduardo Muchak <eduardomuchak@gmail.com>
2022-05-10 15:16:45 -0300

    CSS: Explore Drinks page finalizada

--- src/pages/ExploreDrinks.js
+++ src/pages/ExploreDrinks.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
-import ExplorerIconDrink from '../images/explore2.svg';
+import ExplorerIconDrink from '../images/explore3.svg';
 import { fetchRandomDrinks } from '../services/fetchRandomRecipe';
 
 function ExploreDrinks() {
@@ -28,32 +28,35 @@ function ExploreDrinks() {
     <>
       <Header pageName="Explore Drinks" showProfileImg="true" />
       <main className="flex flex-wrap w-screen justify-center h-24 items-center">
-        <button
-          className="font-bold bg-darkblue font-3xl text-white
-            rounded-xl py-2 px-4 mt-4 opacity-90
-            hover:opacity-100"
-          type="button"
-          data-testid="explore-by-ingredient"
-          onClick={ () => { history.push('drinks/ingredients'); } }
-        >
-          By Ingredient
-        </button>
-        <button
-          className="font-bold bg-darkblue font-3xl text-white
-            rounded-xl py-2 px-4 mt-4 opacity-90
-            hover:opacity-100"
-          type="button"
-          data-testid="explore-surprise"
-          onClick={ () => handleSurprise() }
-        >
-          Surprise me!
-        </button>
-
         <img
-          className="wy-50"
+          className="w-80 mx-auto mt-5 mb-4"
           src={ ExplorerIconDrink }
           alt="logo app"
         />
+        <div className="flex flex-col mx-4 w-full">
+
+          <button
+            className="font-2xl font-semibold mt-5
+            bg-darkblue text-white rounded-xl py-3 opacity-100
+            hover:opacity-90"
+            type="button"
+            data-testid="explore-by-ingredient"
+            onClick={ () => { history.push('drinks/ingredients'); } }
+          >
+            By Ingredient
+          </button>
+          <button
+            className="font-2xl font-semibold mt-2
+            bg-darkblue text-white rounded-xl py-3 opacity-100
+            hover:opacity-90"
+            type="button"
+            data-testid="explore-surprise"
+            onClick={ () => handleSurprise() }
+          >
+            Surprise me!
+          </button>
+
+        </div>
       </main>
       <Footer />
     </>
